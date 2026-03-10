0315fb10196e959e8c7a21d4d499f91abc014f5d
deeksha <155137431+Deeksha20051978@users.noreply.github.com>
2026-03-10 08:46:26 +0530

    Create ComplexityComparision.java
    M7

---
+++ ComplexityComparision.java
@@ -0,0 +1,13 @@
+public class ComplexityComparison {
+
+    public static void main(String args[]) {
+
+        System.out.println("Sorting Algorithm Complexity");
+
+        System.out.println("Bubble Sort -> Time: O(n^2), Space: O(1)");
+        System.out.println("Selection Sort -> Time: O(n^2), Space: O(1)");
+        System.out.println("Insertion Sort -> Time: O(n^2), Space: O(1)");
+        System.out.println("Merge Sort -> Time: O(n log n), Space: O(n)");
+        System.out.println("Quick Sort -> Time: O(n log n), Space: O(log n)");
+    }
+}
