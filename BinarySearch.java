7b30ba5a85af444e035376dffb146abdc45564b8
deeksha <155137431+Deeksha20051978@users.noreply.github.com>
2026-03-10 08:39:09 +0530

    Create BinarySearch.java
    M7

---
+++ BinarySearch.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BinarySearch {
+    
+    static int binarySearchIterative(int arr[], int key) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    static int binarySearchRecursive(int arr[], int low, int high, int key) {
+        if (low > high)
+            return -1;
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] > key)
+            return binarySearchRecursive(arr, low, mid - 1, key);
+        else
+            return binarySearchRecursive(arr, mid + 1, high, key);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2,4,6,8,10,12};
+
+        System.out.println("Iterative Search: " + binarySearchIterative(arr, 8));
+        System.out.println("Recursive Search: " + binarySearchRecursive(arr,0,arr.length-1,8));
+    }
+}
